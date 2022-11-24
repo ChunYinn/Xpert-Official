a4ac640d228a311ce71b55fc7d80aa9547d30884
Meschac Irung <meschacirung@gmail.com>
2022-11-24 10:13:40 +0200

    classes sorting init with prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  
   plugins: [require("prettier-plugin-tailwindcss")],
   tailwindConfig: "./tailwind.config.js",
-};
+}
