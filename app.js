5aea4b2c3071accaad859b6f9c2d36823589da92
Meschac Irung <meschacirung@gmail.com>
2022-12-01 12:30:51 +0200

    optimize for seo ...

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ import "./assets/app.css";
 import "./src/js/faqs.js";
 import "./src/js/theme.js";
 import "./src/js/solutions.js";
+
+const hamburger = document.querySelector('#hamburger')
+const navbar = document.querySelector('#navbar')
+
+hamburger.addEventListener('click', ()=>{
+    navbar.classList.toggle('navbar-active')
+})
