ae379b2c80fd37b9bb40d9f0ed111dea74de6a30
eric <eric.li@predico.com.au>
2024-09-23 12:39:32 +1000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/xpert_official_website/',  // The base path should match your repository name
+  base: '/',
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"), // Only include index.html
-      },
-    },
+    outDir: 'dist',
   },
 });
