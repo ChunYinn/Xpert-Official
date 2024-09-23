5189a4138aea8f83678e5ad356acdb764158333b
sunnxe <s778906@gmail.com>
2024-09-23 10:29:16 +1000

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/xpert_official_website/',  // The base path should match your repository name
   build: {
     rollupOptions: {
       input: {
